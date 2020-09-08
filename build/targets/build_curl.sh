d01e7a966e56b62c0b441cb69292c7082339cd8e
Sebastian Funke <sfunke@ernw.de>
2020-09-08 23:11:32 +0200

    added curl_x86_64

---
+++ build/targets/build_curl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+set -x
+set -o pipefail
+if [ "$#" -ne 1 ];then
+    echo "Usage: ${0} [x86|x86_64|armhf|aarch64]"
+    echo "Example: ${0} x86_64"
+    exit 1
+fi
+source $GITHUB_WORKSPACE/build/lib.sh
+init_lib "$1"
+
+build_curl() {
+    fetch "https://github.com/curl/curl.git" "${BUILD_DIRECTORY}/curl" git
+    cd "${BUILD_DIRECTORY}/curl"
+    git clean -fdx
+    git checkout master
+    ./buildconf
+    ./configure --disable-shared
+    strip curl
+}
+
+main() {
+    build_curl
+    local version
+    version=$(get_version "${BUILD_DIRECTORY}/curl/curl --version 2>&1 | head -n1 | awk '{print \$2}'")
+    cp "${BUILD_DIRECTORY}/curl/curl" "${OUTPUT_DIRECTORY}/curl"
+    echo "[+] Finished building curl ${CURRENT_ARCH}"
+
+    echo ::set-output name=PACKAGED_NAME::"curl${version}"
+    echo ::set-output name=PACKAGED_NAME_PATH::"${OUTPUT_DIRECTORY}/*"
+}
+
+main
